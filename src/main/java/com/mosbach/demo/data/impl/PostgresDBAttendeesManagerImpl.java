4ea844fd5765c36472c038238cbdaefb75b57145
selina fehn <sel.fehn.22@lehre.mosbach.dhbw.de>
2023-10-30 11:18:27 +0100

    added addAttendee()

--- src/main/java/com/mosbach/demo/data/impl/PostgresDBAttendeesManagerImpl.java
+++ src/main/java/com/mosbach/demo/data/impl/PostgresDBAttendeesManagerImpl.java
@@ -117,7 +117,7 @@ public class PostgresDBAttendeesManagerImpl implements AttendeesManager {
         try {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
-            String udapteSQL = "INSERT into users (relID, userID, meetupID, host) VALUES (" +
+            String udapteSQL = "INSERT into attendees (relID, userID, meetupID, host) VALUES (" +
                     "'" + relID +"', " +
                     "'" + userID + "', " +
                     "'" + meetupID + "', " +
