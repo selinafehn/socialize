3de819d234271cf49388966b53ac98181d14ec10
selina fehn <sel.fehn.22@lehre.mosbach.dhbw.de>
2023-11-05 22:55:00 +0100

    changed options endpoint

--- src/main/java/com/mosbach/demo/data/impl/PostgresDBOptionManagerImpl.java
+++ src/main/java/com/mosbach/demo/data/impl/PostgresDBOptionManagerImpl.java
@@ -77,7 +77,7 @@ public class PostgresDBOptionManagerImpl implements OptionsManager {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
 
-            ResultSet rs1 = stmt.executeQuery("SELECT COUNT(*) options WHERE meetupid = " +"'" +meetupid +"'" +"'" );
+            ResultSet rs1 = stmt.executeQuery("SELECT COUNT(*) options WHERE meetupid = " +"'" +meetupid  +"'" );
             int counting = rs1.getInt(1)+1;
 
             String udapteSQL = "INSERT into options (optionid,optionserial, meetupid, dateandtime) VALUES (" +
