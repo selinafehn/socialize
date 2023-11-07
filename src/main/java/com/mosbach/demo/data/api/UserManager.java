2d535b9a1ce5b517fa172837a99958a19dfd9bda
selina fehn <sel.fehn.22@lehre.mosbach.dhbw.de>
2023-11-07 02:50:53 +0100

    commented deleteuser out

--- src/main/java/com/mosbach/demo/data/api/UserManager.java
+++ src/main/java/com/mosbach/demo/data/api/UserManager.java
@@ -13,7 +13,7 @@ public interface UserManager {
     User getUserbyToken(String token);
     SendBackToken logUserIn(String email, String password);
     boolean logUserOff( String token);
-     String deleteUserbyID(String userID);
+     //String deleteUserbyID(String userID);
 
 
 }
