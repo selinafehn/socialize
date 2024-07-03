46e7aeccc491f6eee8db0e5a4f772e99a0db2b85
Simon Siedler <simon.siedler@iwelt.de>
2024-07-03 22:35:39 +0200

    ci: build container

--- src/main/java/com/mosbach/demo/data/impl/PostgresDBUserManagerImpl.java
+++ src/main/java/com/mosbach/demo/data/impl/PostgresDBUserManagerImpl.java
@@ -1,9 +1,11 @@
 package com.mosbach.demo.data.impl;
+
 import com.mosbach.demo.data.api.User;
 import com.mosbach.demo.data.api.UserManager;
 import com.mosbach.demo.model.Userlogin;
 import com.mosbach.demo.model.auth.SendBackToken;
 import org.apache.commons.dbcp.BasicDataSource;
+
 import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,30 +14,37 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 public class PostgresDBUserManagerImpl implements UserManager {
 
-    String databaseURL = "jdbc:postgresql://ec2-3-214-103-146.compute-1.amazonaws.com/ddba3pgnqq5msa";
+//    String jdbcHost = Optional.of(System.getProperty("JDBC_HOST")).filter(s -> !s.trim().equalsIgnoreCase("")).orElseGet(() -> "localhost");
+    String jdbcHost = "localhost";
+
+    String databaseURL = "jdbc:postgresql://" + jdbcHost + ":5432";
     String username = "uiefynxlnqznhz";
     String password = "ba3c282752e67e5d6e0ef420e072f58f6c3c10ec5b179ff195d940efe66e8d1a";
     BasicDataSource basicDataSource;
 
-
     // dass die bytes randomized werden(stack overflow)
+
     private static SecureRandom random = new SecureRandom();
     private static Base64.Encoder encoder = Base64.getUrlEncoder();
 
-    static String sessiontoken ="";
+    static String sessiontoken = "";
 
     // Singleton
     static PostgresDBUserManagerImpl postgresDBUserManager = null;
+
     private PostgresDBUserManagerImpl() {
         basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(databaseURL);
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
     }
+
     public static PostgresDBUserManagerImpl getPostgresDBUserManagerImpl() {
         if (postgresDBUserManager == null)
             postgresDBUserManager = new PostgresDBUserManagerImpl();
@@ -46,10 +55,10 @@ public class PostgresDBUserManagerImpl implements UserManager {
     // Be carefull: It deletes data if table already exists.
 
 
-    public List<User>readAllUsers() {
+    public List<User> readAllUsers() {
 
         final Logger readUserLogger = Logger.getLogger("ReadUserLogger");
-        readUserLogger.log(Level.INFO,"Start reading ");
+        readUserLogger.log(Level.INFO, "Start reading ");
 
         List<User> users = new ArrayList<>();
         Statement stmt = null;
@@ -123,17 +132,17 @@ public class PostgresDBUserManagerImpl implements UserManager {
         try {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email =  '" +email +"'" );
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email =  '" + email + "'");
             if (rs.next()) {
-                       user = new UserImpl(
-                                rs.getString("userid"),
-                                rs.getString("firstname"),
-                                rs.getString("lastname"),
-                                rs.getString("password"),
-                                rs.getString("email"),
-                                rs.getString("token"),
-                                rs.getLong("validuntil")
-                        );
+                user = new UserImpl(
+                        rs.getString("userid"),
+                        rs.getString("firstname"),
+                        rs.getString("lastname"),
+                        rs.getString("password"),
+                        rs.getString("email"),
+                        rs.getString("token"),
+                        rs.getLong("validuntil")
+                );
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -156,7 +165,7 @@ public class PostgresDBUserManagerImpl implements UserManager {
         try {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE token =  '" +token +"'" );
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE token =  '" + token + "'");
             if (rs.next()) {
                 user = new UserImpl(
                         rs.getString("userid"),
@@ -185,21 +194,21 @@ public class PostgresDBUserManagerImpl implements UserManager {
     @Override
     public User createUser(String userID, String firstName, String lastName, String password, String email, String token, long validUntil) {
         final Logger createUserLogger = Logger.getLogger("CreateUserLogger");
-        createUserLogger.log(Level.INFO,"Start creating " + email);
+        createUserLogger.log(Level.INFO, "Start creating " + email);
         Statement stmt = null;
         Connection connection = null;
         try {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
             String udapteSQL = "INSERT into users (userID, firstname, lastname, password, email, token, validUntil) VALUES (" +
-                    "'" + userID +"', " +
+                    "'" + userID + "', " +
                     "'" + firstName + "', " +
                     "'" + lastName + "', " +
                     "'" + password + "', " +
                     "'" + email + "', " +
                     "'" + token + "', " +
-                    validUntil +")";
-            Logger.getLogger("DbUSerManager").log(Level.INFO,udapteSQL);
+                    validUntil + ")";
+            Logger.getLogger("DbUSerManager").log(Level.INFO, udapteSQL);
 
             stmt.executeUpdate(udapteSQL);
             stmt.close();
@@ -220,20 +229,20 @@ public class PostgresDBUserManagerImpl implements UserManager {
     @Override
     public SendBackToken logUserIn(String email, String password) {
         final Logger loginUserLogger = Logger.getLogger("LoginUserLogger");
-        loginUserLogger.log(Level.INFO,"Start logging in " + email);
+        loginUserLogger.log(Level.INFO, "Start logging in " + email);
         Statement stmt = null;
         Connection connection = null;
         List<User> user = readAllUsers();
         User testuser = null;
 
-        for (User u : user){
-            if (u.getEmail().equals(email)){
+        for (User u : user) {
+            if (u.getEmail().equals(email)) {
                 testuser = u;
             }
         }
         if (!testuser.getPassword().equals(password)) return null;
         //token generation
-        long validUntil = (System.currentTimeMillis()+(1800*1000));
+        long validUntil = (System.currentTimeMillis() + (1800 * 1000));
         byte[] tokenbyte = new byte[16];
         random.nextBytes(tokenbyte);
         String token = encoder.encodeToString(tokenbyte);
@@ -242,10 +251,10 @@ public class PostgresDBUserManagerImpl implements UserManager {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
             String udapteSQL = "UPDATE users SET token = " +
-                    "'" +token +"', " +
+                    "'" + token + "', " +
                     "validuntil= " + validUntil +
-                    " WHERE userid = '" + testuser.getUserID() +"'";
-            Logger.getLogger("DbUSerManager").log(Level.INFO,udapteSQL);
+                    " WHERE userid = '" + testuser.getUserID() + "'";
+            Logger.getLogger("DbUSerManager").log(Level.INFO, udapteSQL);
 
             stmt.executeUpdate(udapteSQL);
             stmt.close();
@@ -262,21 +271,21 @@ public class PostgresDBUserManagerImpl implements UserManager {
 
         sessiontoken = token;
         UserSession session = new UserSession(testuser);
-       return new SendBackToken(token, validUntil, testuser.getUserID());
+        return new SendBackToken(token, validUntil, testuser.getUserID());
     }
 
     public String getUserIDByEmail(String email) {
         final Logger readUserLogger = Logger.getLogger("ReadUserLogger");
-        readUserLogger.log(Level.INFO,"Start reading ");
+        readUserLogger.log(Level.INFO, "Start reading ");
         Statement stmt = null;
         Connection connection = null;
         try {
             connection = basicDataSource.getConnection();
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email = "+email );
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email = " + email);
             String userID = rs.getNString(0);
             final Logger rLogger = Logger.getLogger("ReadUserLogger");
-            readUserLogger.log(Level.INFO,"Start reading " +userID);
+            readUserLogger.log(Level.INFO, "Start reading " + userID);
             return userID;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -294,14 +303,14 @@ public class PostgresDBUserManagerImpl implements UserManager {
     public boolean logUserOff(String token) {
 
         final Logger loginOffLogger = Logger.getLogger("LogoffUserLogger");
-        loginOffLogger.log(Level.INFO,"Start logging off ");
+        loginOffLogger.log(Level.INFO, "Start logging off ");
 
         List<User> userlist = readAllUsers();
 
         User testuser = null;
 
-        for (User u : userlist){
-            if (u.getToken().equals(token)){
+        for (User u : userlist) {
+            if (u.getToken().equals(token)) {
                 testuser = u;
             }
         }
@@ -318,9 +327,9 @@ public class PostgresDBUserManagerImpl implements UserManager {
             String udapteSQL = "UPDATE users SET token = " +
                     "' logged off ', " +
                     "validuntil= " + 0 +
-                    " WHERE userid = '" + testuser.getUserID() +"'";
+                    " WHERE userid = '" + testuser.getUserID() + "'";
 
-            Logger.getLogger("DbUSerManager").log(Level.INFO,udapteSQL);
+            Logger.getLogger("DbUSerManager").log(Level.INFO, udapteSQL);
 
             stmt.executeUpdate(udapteSQL);
             stmt.close();
@@ -339,33 +348,32 @@ public class PostgresDBUserManagerImpl implements UserManager {
 
 
     /**
-    @Override
-    public String deleteUserbyID(String userID) {
-        final Logger createUserLogger = Logger.getLogger("CreateUserLogger");
-        createUserLogger.log(Level.INFO,"Start deleting " );
-        Statement stmt = null;
-        Connection connection = null;
-        try {
-            connection = basicDataSource.getConnection();
-            stmt = connection.createStatement();
-            String deleteSQL = "DELETE FROM users WHERE userid = '" +userID +"'";
-            Logger.getLogger("DbUSerManager").log(Level.INFO,deleteSQL);
-            stmt.executeUpdate(deleteSQL);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return
-                "deleted " +userID;
-    }
+     @Override public String deleteUserbyID(String userID) {
+     final Logger createUserLogger = Logger.getLogger("CreateUserLogger");
+     createUserLogger.log(Level.INFO,"Start deleting " );
+     Statement stmt = null;
+     Connection connection = null;
+     try {
+     connection = basicDataSource.getConnection();
+     stmt = connection.createStatement();
+     String deleteSQL = "DELETE FROM users WHERE userid = '" +userID +"'";
+     Logger.getLogger("DbUSerManager").log(Level.INFO,deleteSQL);
+     stmt.executeUpdate(deleteSQL);
+     stmt.close();
+     connection.close();
+     } catch (SQLException e) {
+     e.printStackTrace();
+     }
+     try {
+     stmt.close();
+     connection.close();
+     } catch (SQLException e) {
+     e.printStackTrace();
+     }
+     return
+     "deleted " +userID;
+     }
 
-    */
+     */
 
 }
