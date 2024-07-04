eeb57c0d45d302b209d1e31ba8e92a8cf60756c8
Simon Siedler <simon.siedler@iwelt.de>
2024-07-04 07:15:19 +0200

    feat: database host selectable by environment variable

--- src/main/java/com/mosbach/demo/data/impl/PostgresDBUserManagerImpl.java
+++ src/main/java/com/mosbach/demo/data/impl/PostgresDBUserManagerImpl.java
@@ -5,6 +5,7 @@ import com.mosbach.demo.data.api.UserManager;
 import com.mosbach.demo.model.Userlogin;
 import com.mosbach.demo.model.auth.SendBackToken;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.LoggerFactory;
 
 import java.security.SecureRandom;
 import java.sql.Connection;
@@ -20,12 +21,8 @@ import java.util.logging.Logger;
 
 public class PostgresDBUserManagerImpl implements UserManager {
 
-//    String jdbcHost = Optional.of(System.getProperty("JDBC_HOST")).filter(s -> !s.trim().equalsIgnoreCase("")).orElseGet(() -> "localhost");
-    String jdbcHost = "localhost";
 
-    String databaseURL = "jdbc:postgresql://" + jdbcHost + ":5432";
-    String username = "uiefynxlnqznhz";
-    String password = "ba3c282752e67e5d6e0ef420e072f58f6c3c10ec5b179ff195d940efe66e8d1a";
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(PostgresDBUserManagerImpl.class);
     BasicDataSource basicDataSource;
 
     // dass die bytes randomized werden(stack overflow)
@@ -40,6 +37,18 @@ public class PostgresDBUserManagerImpl implements UserManager {
 
     private PostgresDBUserManagerImpl() {
         basicDataSource = new BasicDataSource();
+
+        String jdbcHost = "localhost";
+        String envKey = "JDBC_HOST";
+        if (System.getenv(envKey) != null) {
+            jdbcHost = System.getenv(envKey);
+        } else if (System.getProperty(envKey) != null) {
+            jdbcHost = System.getProperty(envKey);
+        }
+        String databaseURL = "jdbc:postgresql://" + jdbcHost + ":5432";
+        log.info("database connection URL: " + databaseURL);
+        String username = "uiefynxlnqznhz";
+        String password = "ba3c282752e67e5d6e0ef420e072f58f6c3c10ec5b179ff195d940efe66e8d1a";
         basicDataSource.setUrl(databaseURL);
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
